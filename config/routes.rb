9b1541a04fc45f0f8c87d1befe6dabfc93f68b12
Ren Simmons <rensimmons@Rens-MacBook-Pro.local>
2016-09-26 22:21:54 -0500

    Add ping route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :entries, except: [:new, :edit]
+  get '/ping', to: 'ping#index'
   scope '/' do
     root 'ember#index'
     mount Rack::File.new('./ember/dist/assets') => '/assets'
