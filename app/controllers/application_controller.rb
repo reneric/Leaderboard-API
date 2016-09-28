291440cdb221e2bc84482c59bedd244e6b6c5b3a
Ren Simmons <rensimmons@Rens-MacBook-Pro.local>
2016-09-27 20:54:07 -0500

    temp remove auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   include ActionController::ImplicitRender
   include ActionController::ParamsWrapper
-  before_action :authenticate
+  # before_action :authenticate
   respond_to :json
   TOKEN = Rails.configuration.api_authorization_token
 
